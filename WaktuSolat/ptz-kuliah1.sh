9bc93495365c8bae68f3979a48f8eae010b7c58c
aminbp <amin.afip@gmail.com>
2020-04-14 17:51:08 +0800

    Delete ptz-kuliah1.sh

--- WaktuSolat/ptz-kuliah1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#lynx -dump "http://<ip address>/cgi-bin/ptzctrl.cgi?ptzcmd&poscall&<preset camera no>" | mail -s "PTZ Kuliah pada $(date)" <insert email here>
-#uncomment to use sample below to switch at PTZ preset 1
-#lynx -dump "http://192.168.2.88/cgi-bin/ptzctrl.cgi?ptzcmd&poscall&1"
